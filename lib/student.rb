91dcfac827237079fc627b3cd5711d7e36f8e9a5
Abel Kalum <chumbabel@gmail.com>
2020-04-03 21:49:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    student_new = self.new(row[0], row[1], row[2])
+    student = Student.new(row[1],row[2], row[0])
   end
   
   def self.find_by_name(name)
