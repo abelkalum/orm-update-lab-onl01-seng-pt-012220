422f578b226991db71bca2c6e35377c55803034b
Abel Kalum <chumbabel@gmail.com>
2020-04-03 20:13:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,14 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  attr_accessor :name, :album
+  attr_reader :id
+ 
+  def initialize(id=nil, name, album)
+    @id = id
+    @name = name
+    @album = album
+  end
 
 
 end
