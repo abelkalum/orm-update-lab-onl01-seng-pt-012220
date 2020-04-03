10e0e5c11ed957b8dcfc0c3a78f5c7867db03ee4
Abel Kalum <chumbabel@gmail.com>
2020-04-03 20:25:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,10 +42,9 @@ class Student
    end
   end
   
-  
-  
-  
-  
-
+  def update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.album, self.id)
+  end
 
 end
